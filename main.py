3010c6d9386e113d73b23543c6b5903134d6bd86
RajeshThakur1 <rajeshthakur1r@gmail.com>
2022-09-24 11:43:48 +0530

    added the main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import pymongo
+
+DEFAULT_CONNECTION_URL = "mongodb://localhost:27017/"
+DB_NAME = "pybron_1"
+client = pymongo.MongoClient(DEFAULT_CONNECTION_URL)
+
+#create DB
+database = client[DB_NAME]
+
+record = {
+    'company_name':"pybron",
+    "product":"Affordable_AI",
+    'course_offered': "deep learning for computer vission"
+}
+collection_name = "pybron_product"
+collection = database[collection_name]
+
+collection.insert_one(record)
